fd2e383cabbd69f9589b41411125a0527d35bfdc
paul grubbs <paulgrub@umail.iu.edu>
2012-03-12 01:07:21 -0400

    hashmap.cc initial commit

---
+++ hashmap.cc
@@ -0,0 +1,45 @@
+#include "hashmap.h"
+
+
+
+
+Bucket::~Bucket(){
+
+	delete elements;
+}
+
+Bucket::Bucket() 
+{
+	std::cout << "Created!" << std::endl;
+	elements = new ElementList;
+}
+
+
+HashMap::HashMap(uint32_t numberOfBuckets) : m_numberOfBuckets(numberOfBuckets)
+{
+
+	buckets = new Bucket[m_numberOfBuckets];
+}
+
+
+HashMap::~HashMap(){
+	delete[] buckets;
+}
+
+uint32_t HashMap::getNumBuckets(){
+	return m_numberOfBuckets;
+}
+
+uint32_t HashMap::lookupKey(uint32_t key){
+	std::cout << "in lookupkey" << std::endl;
+	return 0;
+}
+
+uint32_t HashMap::addElement(uint32_t key, uint32_t value){
+	std::cout << "in addelement" << std::endl;
+	return 0;
+}
+bool HashMap::removeElement(uint32_t key){
+	std::cout << "in removeelement" << std::endl;
+	return true;
+}
